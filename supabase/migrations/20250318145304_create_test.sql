bfb4588a6b8a751244ad2fb00045b6a27aa70a49
Henry Firth <contact@henrynelsonfirth.com>
2025-03-23 15:58:08 -0600

    Formatting

--- supabase/migrations/20250318145304_create_test.sql
+++ supabase/migrations/20250318145304_create_test.sql
@@ -2,8 +2,8 @@
 create table test (
     id serial primary key,
     random_number int2 not null default 37,
-    created_at timestamp default now (),
-    updated_at timestamp default now ()
+    created_at timestamp default now(),
+    updated_at timestamp default now()
 );
 
 -- Turn on security
@@ -11,5 +11,4 @@ alter table "test" enable row level security;
 
 -- Allow anonymous access
 create policy "Allow anonymous access" on test for
-select
-    to anon using (true);
+select to anon using (true);
